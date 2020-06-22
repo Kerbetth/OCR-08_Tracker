a181747854f673a5f0a1591c2f51aa8cdce92456
dufeu <kerbetth@protonmail.com>
2020-06-22 16:27:06 +0200

    correction sur la methode getnearbyAttraction

--- TourGuide_TrackerService/src/main/java/tourGuideTracker/controller/TrackerController.java
+++ TourGuide_TrackerService/src/main/java/tourGuideTracker/controller/TrackerController.java
@@ -5,15 +5,12 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import tourGuideTracker.domain.FiveNearestAttractions;
-import tourGuideTracker.domain.User;
-import tourGuideTracker.domain.UserLocation;
 import tourGuideTracker.domain.VisitedLocation;
 import tourGuideTracker.domain.location.Location;
 import tourGuideTracker.service.TrackerService;
@@ -43,13 +40,13 @@ public class TrackerController {
     // The distance in miles between the user's location and each of the attractions.
     // The reward points for visiting each Attraction.
     //    Note: Attraction reward points can be gathered from RewardsCentral
-    @RequestMapping("/getFiveNearbyAttractions")
+    @RequestMapping("/get5NearestAttractions")
     public FiveNearestAttractions get5NearestAttractions(@RequestParam Location location) {
         return trackerService.get5NearestAttractions(location);
     }
 
     @RequestMapping("/getAllCurrentLocations")
-    public Map<UUID, Location> getAllCurrentLocations() {
+    public Map<UUID, Location> getAllCurrentLocations(List<UUID> userId) {
         // TODO: Get a list of every user's most recent location as JSON
         //- Note: does not use gpsUtil to query for their current location,
         //        but rather gathers the user's current location from their stored location history.
@@ -59,6 +56,6 @@ public class TrackerController {
         //        "019b04a9-067a-4c76-8817-ee75088c3822": {"longitude":-48.188821,"latitude":74.84371}
         //        ...
         //     }
-        return trackerService.getLocationOfAllUsers();
+        return trackerService.getCurrentLocationOfAllUsers(userId);
     }
 }
